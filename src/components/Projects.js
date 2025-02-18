8ca771a831c46bc65123ba3d9aa4cd8a456d5bfc
evya5 <evyatarc555@gmail.com>
2025-02-18 16:42:27 +0200

    More progrees in the Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,51 +1,54 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Projects.css';
 import cooperVrStatic from '../assets/cooper-vr-static.png';
-
+import trashDayStatic from '../assets/trash-day-static.png';
+import breakingNewsGif from '../assets/breaking-news-gif.gif';
+import swipeToUnlockStatic from '../assets/swipe-to-unlock-static.png';
+import vertigoGift from '../assets/vertigo-gif1.gif';
 
 const projectsData = [
   {
     id: 'breaking-news',
     title: 'Breaking News',
-    staticImage: 'https://via.placeholder.com/200?text=Breaking+News',
-    gifImage: 'https://via.placeholder.com/200?text=Breaking+News+Gif',
-    description: 'A short description for Breaking News.',
+    category: 'Award Winning Interactive Experience',
+    gifImage: breakingNewsGif,
+    description: 'Award Winning Nostalgic Interactive Experience',
   },
   {
     id: 'cooper-vr',
     title: 'Cooper VR',
-    staticImage: cooperVrStatic,
-    gifImage: 'https://via.placeholder.com/200?text=Cooper+VR+Gif',
-    description: 'A short description for Cooper VR.',
+    category: 'Funded VR & AI Project',
+    gifImage: cooperVrStatic,
+    description: 'Funded academic project that uses VR and AI to practice social interaction.',
   },
   {
     id: 'galaxy-eggs',
     title: 'Galaxy Eggs',
-    staticImage: 'https://via.placeholder.com/200?text=Galaxy+Eggs',
-    gifImage: 'https://via.placeholder.com/200?text=Galaxy+Eggs+Gif',
-    description: 'A short description for Galaxy Eggs.',
+    category: 'Top-Tier NFT Art Collection',
+    gifImage: 'https://static.wixstatic.com/media/d55833_75b7129dd84840b2aec8954490154ab4~mv2.jpg/v1/fill/w_1380,h_1237,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/d55833_75b7129dd84840b2aec8954490154ab4~mv2.jpg',
+    description: 'Top-tier art collection featuring 3D rendered universes by Gal Barkan',
   },
   {
     id: 'trash-day',
     title: 'Trash Day',
-    staticImage: 'https://via.placeholder.com/200?text=Trash+Day',
-    gifImage: 'https://via.placeholder.com/200?text=Trash+Day+Gif',
-    description: 'A short description for Trash Day.',
+    category: 'Casual Endless-Runner Mobile Game',
+    gifImage: trashDayStatic,
+    description: 'Casual Mobile for high-paced street cleaning',
   },
   {
     id: 'swipe-to-unlock',
     title: 'Swipe to Unlock',
-    staticImage: 'https://via.placeholder.com/200?text=Swipe+to+Unlock',
-    gifImage: 'https://via.placeholder.com/200?text=Swipe+to+Unlock+Gif',
-    description: 'A short description for Swipe to Unlock.',
+    category: 'Social Horror Game',
+    gifImage: swipeToUnlockStatic,
+    description: 'Social Game for brave people who got nothing to hide',
   },
   {
     id: 'vertigo',
     title: 'Vertigo',
-    staticImage: 'https://via.placeholder.com/200?text=Vertigo',
-    gifImage: 'https://via.placeholder.com/200?text=Vertigo+Gif',
-    description: 'A short description for Vertigo.',
+    category: 'Endless Runner Game',
+    gifImage: vertigoGift,
+    description: 'Outmaneuver creeping spiders and hungry plants to light up your escape from the dark greenhouse',
   },
 ];
 
@@ -66,17 +69,18 @@ const Projects = () => {
 };
 
 const ProjectCard = ({ project, onClick }) => {
-  const [imgSrc, setImgSrc] = useState(project.staticImage);
-
   return (
     <div className="project-card" onClick={() => onClick(project.id)}>
-      <img
-        src={imgSrc}
-        alt={project.title}
-        onMouseEnter={() => setImgSrc(project.gifImage)}
-        onMouseLeave={() => setImgSrc(project.staticImage)}
-      />
-      <p>{project.description}</p>
+      <div className="project-thumbnail">
+        <img src={project.gifImage} alt={project.title} loading="lazy" />
+        <div className="overlay">
+          <p>{project.description}</p>
+        </div>
+      </div>
+      <div className="project-info">
+        <h3 className="project-title">{project.title}</h3>
+        <p className="project-category">{project.category}</p>
+      </div>
     </div>
   );
 };
