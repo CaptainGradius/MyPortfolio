0d2cf13eaf7a38d4b3d775760708bf9e57957622
evya5 <evyatarc555@gmail.com>
2025-03-07 15:49:07 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
       <div className="footer-left">Works by Evyatar Cohen</div>
-      <div className="footer-right">©2025</div>
+      <div className="footer-right">©2021-{currentYear}</div>
     </footer>
   );
 };
